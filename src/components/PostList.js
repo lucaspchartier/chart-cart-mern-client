cd757da60ec5a7805e14c046202fc6e26bc367bb
lucaspchartier <lucaspchartier@gmail.com>
2021-08-24 07:43:29 -0400

    chore(src/components/PostList.js): add post placeholder before dynamically rendering each post made

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class PostList extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <ul>
+          <li>Post</li>
+        </ul>
+      </div>
     );
   }
 }
