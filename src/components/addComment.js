e883cd4a5564bd89022ff09b526ee110e6300840
lucaspchartier <lucaspchartier@gmail.com>
2021-08-31 20:53:03 -0400

    chore(src/components/addComment.js): add constructor() function to addComment component and set this.state to default values of postId and text for a comment

--- src/components/addComment.js
+++ src/components/addComment.js
@@ -1,2 +1,12 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+
+class AddComment extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            postId: "",
+            text: ""
+        }
+    }
+}
