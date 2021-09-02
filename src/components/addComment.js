adda475b4eb98942eda2cbb747dffb41700f0c4e
lucaspchartier <lucaspchartier@gmail.com>
2021-09-02 07:43:44 -0400

    chore(src/components/addComment.js): add onSubmit prop and bind this to event being passed in

--- src/components/addComment.js
+++ src/components/addComment.js
@@ -35,8 +35,8 @@ class AddComment extends Component {
 
     render() {
         return (
-            <form id="add-comment">
-                
+            <form id="add-comment" onSubmit={ this.submitForm.bind(this) }>
+
             </form>
         );
     }
