9bed079eea3d7509a8b0f6c2f0bdce39dedf02d6
lucaspchartier <lucaspchartier@gmail.com>
2021-09-03 07:51:15 -0400

    fix(src/components/AddComment.js): add capital to AddComment.js file in components directory

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 import PostList from "./components/PostList";
-import addComment from "./components/addComment";
+import AddComment from "./components/addComment";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -16,7 +16,7 @@ class App extends Component {
         <div className="App">
           <h1>Chart Cart</h1>
           <PostList/>
-          <addComment/>
+          <AddComment/>
         </div>
       </ApolloProvider>
     );
