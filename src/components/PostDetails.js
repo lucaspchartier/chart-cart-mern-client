79563d92e34d07921e8eb8ec386fab6553e6c7f0
lucaspchartier <lucaspchartier@gmail.com>
2021-09-07 07:16:30 -0400

    chore(src/components/PostDetails.js): begin to construct PostDetails component
    use object destructuring to set book to this.props.data

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,2 +1,9 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+
+class PostDetails extends Component {
+    displayBookDetails() {
+        const { post } = this.props.data;
+        
+    }
+}
