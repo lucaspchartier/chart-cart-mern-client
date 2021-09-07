482274717e9f6c3e9d5182d0c5cf1e637ff2847d
lucaspchartier <lucaspchartier@gmail.com>
2021-09-07 07:20:39 -0400

    chore(src/components/PostDetails.js): if a post exists, render its text and its comments

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -4,6 +4,13 @@ import { graphql } from "react-apollo";
 class PostDetails extends Component {
     displayBookDetails() {
         const { post } = this.props.data;
-        
+        if (post) {
+            return (
+                <div>
+                    <h2>{post.text}</h2>
+                    <h2>{post.comment.text}</h2>
+                </div>
+            )
+        }
     }
 }
