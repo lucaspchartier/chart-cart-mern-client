495f7f557a3334687387a42604177aa41f425ee7
lucaspchartier <lucaspchartier@gmail.com>
2021-09-08 20:11:08 -0400

    chore(src/queries/queries.js): add addPostMutation graphql query

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -48,6 +48,15 @@ const getCommentQuery = gql`
     }
 `
 
+const addPostMutation = gql`
+    mutation($text: String!) {
+        addPost(text: $text) {
+            id
+            text
+        }
+    }
+`
+
 const addCommentMutation = gql`
     mutation($text: String!, $postId: ID!) {
         addComment(text: $text, postId: $postId) {
