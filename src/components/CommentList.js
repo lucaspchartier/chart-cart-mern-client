a28ae3e0deba31d97dc57bca3a4d75e6873abb09
lucaspchartier <lucaspchartier@gmail.com>
2021-09-14 09:18:53 -0400

    chore(src/components/CommentList.js): create getComments query

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,3 +1,10 @@
 import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
+
+const getCommentsQuery = gql`
+    comments {
+        id
+        text
+    }
+`
