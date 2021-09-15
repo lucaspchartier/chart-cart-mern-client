5684050cc19fd8732a02a7a45fa275df474ff26c
lucaspchartier <lucaspchartier@gmail.com>
2021-09-15 08:35:48 -0400

    chore(src/components/CommentList.js): render all comments being displayed

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -27,4 +27,12 @@ class CommentList extends Component {
             });
         }
     };
+
+    render() {
+        return (
+            <div>
+                {this.displayComments()}
+            </div>
+        );
+    }
 }
