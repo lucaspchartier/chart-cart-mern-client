d8fe0fb8cac4548e0e4495708c9cffdf6a874098
lucaspchartier <lucaspchartier@gmail.com>
2021-09-26 20:36:24 -0400

    fix(src/componentsPostList.js): uncomment out import statement for CommentList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
 
-// import CommentList from "./CommentList";
+import CommentList from "./CommentList";
 
 const getPostsQuery = gql`
   {
