ffbbdbe13f4658992bb1698ca0bbc97a95694444
lucaspchartier <lucaspchartier@gmail.com>
2021-10-05 20:54:19 -0400

    fix(src/components/CommentList.js): remove commented out and redundant graphql comment query

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,13 +3,6 @@ import React, { Component } from "react";
 import AddComment from "./AddComment";
 import './CommentList.css';
 
-// const getCommentsQuery = gql`
-//     comments {
-//         id
-//         text
-//     }
-// `
-
 class CommentList extends Component {
     displayComments() {
         const comments = this.props.comments;
