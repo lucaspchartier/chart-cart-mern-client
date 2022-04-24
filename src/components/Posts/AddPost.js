9b167c44d35d9d9da22521bc673fb8f503bd832d
lucaspchartier <lucaspchartier@gmail.com>
2022-04-24 18:19:54 -0400

    chore(src/components/Posts/AddPost.js): import useState() function from React library in import statement

--- src/components/Posts/AddPost.js
+++ src/components/Posts/AddPost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql } from "react-apollo";
 import flowright from "lodash.flowright";
 import { getPostsQuery, addPostMutation } from "../../queries/queries";
