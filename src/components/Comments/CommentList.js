7104323619e197992e504d333b40782a55d1e177
lucaspchartier <lucaspchartier@gmail.com>
2022-07-28 22:25:55 -0400

    fix(src/components/Comments/CommentList.js): move selectedPost Hook above comments Hook in CommentList.js component

--- src/components/Comments/CommentList.js
+++ src/components/Comments/CommentList.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-
 import AddComment from "./AddComment";
 import './CommentList.css';
 
 export const CommentList = props => {
-    const [comments, setComments] = useState([]);
     const [selectedPost, setSelectedPost] = useState(0);
+    const [comments, setComments] = useState([]);
 
     const displayComments = () => {
         const comments = props.comments;
         return comments.map(comment => {
             return <ul key={comment.id} onClick={e => {
-                setComments(props.comments);
                 setSelectedPost(props.selectedPost);
+                setComments(props.comments);
             }}><li id="comment">{comment.text}</li></ul>
         });
     }
