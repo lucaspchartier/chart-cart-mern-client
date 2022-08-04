ac13dfa2a4e825dce5dc1ebbece709b237eac608
lucaspchartier <lucaspchartier@gmail.com>
2022-08-04 07:27:44 -0400

    fix(src/components/Comments/AddComment.js): set default value of postId property in allCommentValues object to 0 instead of empty string as postId is an integer value

--- src/components/Comments/AddComment.js
+++ src/components/Comments/AddComment.js
@@ -5,7 +5,7 @@ import { getPostsQuery, getCommentsQuery, addCommentMutation } from "../../queri
 
 export const AddComment = props => {
     const [allCommentValues, setAllCommentValues] = useState({
-        postId: "",
+        postId: 0,
         commentTextFields: ""
     });
 
@@ -51,7 +51,7 @@ export const AddComment = props => {
                     type="text"
                     label="Text"
                     placeholder="comment"
-                    onChange={handleChangedCommentValues}
+                    onChange={event => handleChangedCommentValues(event, props.postId)}
                 />
             </div>
 
