0d9ba7b8c74d3f1c766573453a73e7c9aa274e5c
lucaspchartier <lucaspchartier@gmail.com>
2022-10-14 23:23:12 -0400

    fix(src/components/Posts/PostList.js): set setSelectedPost() method to post id within displayPosts() function

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -7,11 +7,7 @@ import './PostList.css';
 import { getPostsQuery } from "../../queries/queries";
 
 export const PostList = props => {
-    const [post, setPost] = useState({
-        id: 0,
-        text: "",
-        comments: []
-    });
+    const [selectedPost, setSelectedPost] = useState(null);
 
     const displayPosts = () => {
         const data = props.data;
@@ -19,11 +15,7 @@ export const PostList = props => {
             return (<div>Loading...</div>);
         } else {
             return data.posts.map(post => {
-                return <div key={post.id} onClick={e => setPost({
-                    id: post.id,
-                    text: post.text,
-                    comments: post.comments
-                })}>{post.text}</div>
+                return <div key={post.id} onClick={e => setSelectedPost(post.id)}>{post.text}</div>
             });
         }
     };
