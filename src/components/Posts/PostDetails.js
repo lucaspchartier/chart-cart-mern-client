d7a074ead7539a99006d04cce8d34fa135736968
lucaspchartier <lucaspchartier@gmail.com>
2022-10-20 22:36:50 -0400

    chore(src/compoennts/Posts/PostDetails.js): add if/else statement to display all comments of a selected post within PostDetails.js component

--- src/components/Posts/PostDetails.js
+++ src/components/Posts/PostDetails.js
@@ -6,7 +6,11 @@ import CommentList from "../Comments/CommentList";
 export const PostDetails = props => {
     const displayPostDetails = () => {
         const { post } = props.data;
-        
+        if (post) {
+            return post.comments.map(comment => <ul key={comment.id}>{comment.text}</ul>);
+        } else {
+            return <div>No post selected...</div>
+        }
     }
 
     return (
